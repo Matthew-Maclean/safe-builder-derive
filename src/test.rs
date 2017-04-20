0312ce9b8b382452c0bbf6622d7d60389ad6b9d2
Matthew Maclean <Matthew Maclean>
2017-04-20 20:54:48 -0300

    delete everything in fit of rage

--- src/test.rs
+++ src/test.rs
@@ -32,39 +32,5 @@ macro_rules! syn_type {
 #[test]
 fn test_complete_struct()
 {
-    let input =
-    "
-    struct S
-    {
-        a: A,
-        b: B,
-        c: C,
-        d: D,
-    }
-    ";
-
-    let macro_input = syn::parse_macro_input(input).unwrap();
-
-    let actual = CompleteStruct::new(&macro_input);
-
-    let expected = CompleteStruct
-    {
-        name: "S".to_owned(),
-        fields:
-        {
-            let mut h = HashMap::new();
-
-            h.insert("a".to_owned(), syn_type!("A"));
-            h.insert("b".to_owned(), syn_type!("B"));
-            h.insert("c".to_owned(), syn_type!("C"));
-            h.insert("d".to_owned(), syn_type!("D"));
-            h
-        }
-    };
-
-    assert_eq!(expected, actual);
-
-    let partials = actual.partials();
-
-    assert!(partials.len() == 16);
+    unimplemented!()
 }
